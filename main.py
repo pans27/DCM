7ead2d8cb5508d920f115dacbeab2c82a137dc77
pans27 <pans27@mcmaster.ca>
2021-10-12 17:59:21 -0400

    gui test

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from tkinter import *
+from tkinter import messagebox
 root=Tk()
+root.title("Pacemaker User Terminal")
+root.geometry("500x300+100+100")
+login=Button(root)
+login["text"]="Login"
+login.pack()
+register=Button(root)
+register["text"]="Register"
+register.pack()
+def loginPressed(e):
+    messagebox.showinfo("Message","type below")
+
+login.bind("<Button-1>",loginPressed)
+register.bind("<Button-1>",loginPressed)
 root.mainloop()
