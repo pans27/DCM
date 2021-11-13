caafb0f32e608b0c092a42d12ea32f445afd1931
yuj168 <71107362+yuj168@users.noreply.github.com>
2021-11-14 01:57:03 +0800

    Update aoor.py

---
+++ parametersA2/aoor.py
@@ -0,0 +1,146 @@
+# November 28, 2021
+# Author: Group 5
+# 3k04 DCM assignment 2 aoor parameters
+
+class Aoor:
+    def __init__(self):
+        self.__lrl = 60
+        self.__url = 120
+        self.__msr = 120
+        self.__aaReg = 3.5
+        self.__aaUnreg = 3.75
+        self.__apw = 0.4
+        self.__at = 4 #default is med, high is 8, med is 4, low is 2
+        self.__reactT = 30000 #in ms (i.e. 30s)
+        self.__rf = 8
+        self.__recovT = 300000 #in ms (i.e. 5min)
+
+    def getLRL(self):
+        return self.__lrl
+
+    def getURL(self):
+        return self.__url
+
+    def getAAReg(self):
+        return self.__aaReg
+
+    def getAAUnreg(self):
+        return self.__aaUnreg
+
+    def getAPW(self):
+        return self.__apw
+
+    def getAT(self):
+        return self.__at
+
+    def getREACT(self):
+        return self.__reactT
+
+    def getRF(self):
+        return self.__rf
+
+    def getRECOVT(self):
+        return self.__recovT
+
+    def setLRL(self, val):
+        if (self.__is_num(val)):
+            num = 5 * round(float(val) / 5)
+            if (round(float(val)) <= 90 and round(float(val)) >= 50):
+                self.__lrl = round(float(val))
+            elif ((num <= 50 and num >= 30) or (num <= 175 and num >= 90)):
+                self.__lrl = num
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def setURL(self, val):
+        if (self.__is_num(val)):
+            num = 5 * round(float(val) / 5)
+            if (num <= 175 and num >= 50):
+                self.__url = num
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def setAAReg(self, val):
+        if (self.__is_num(val)):
+            num = 0.5 * round(float(val) / 0.5)
+            if (round(float(val), 1) <= 3.2 and round(float(val), 1) >= 0.5):
+                self.__aaReg = round(float(val), 1)
+            elif (num <= 7.0 and num >= 3.5):
+                self.__aaReg = num
+            elif (round(float(val), 1) == 0):
+                self.__aaReg = 0
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def setAAUnreg(self, val):
+        if (self.__is_num(val)):
+            num = 1.25 * round(float(val) / 1.25)
+            if (round(float(val), 2) <= 5.00 and round(float(val), 2) >= 1.25):
+                self.__aaReg = num
+            elif (round(float(val), 1) == 0):
+                self.__aa = 0
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def setAPW(self, val):
+        if (self.__is_num(val)):
+            if (round(float(val), 1) <= 1.9 and round(float(val), 1) >= 0.1):
+                self.__apw = float(val)
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def setAT(self,val):
+        if (self.__is_num(val)):
+            num = round(float(val))
+            if (num != 8 and num != 4 and num != 2):
+                raise IndexError
+            else:
+                self.__at = num
+        else:
+            raise TypeError
+
+    def setREACT(self,val):
+        if (self.__is_num(val)):
+            num = 10 * round(float(val) / 10)
+            if (num <= 50 and num >= 10):
+                self.__reactT = num*1000 #in ms
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def setRF(self,val):
+        if (self.__is_num(val)):
+            if (round(float(val)) <= 16 and round(float(val)) >= 1):
+                self.__rf = round(float(val))
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def setRECOVT(self,val):
+        if (self.__is_num(val)):
+            if ( round(float(val)) <= 16 and round(float(val)) >= 2):
+                self.__recovT = round(float(val))*60000 #in ms
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def __is_num(self, s):
+        try:
+            float(s)
+        except ValueError:
+            return False
+        else:
+            return True
