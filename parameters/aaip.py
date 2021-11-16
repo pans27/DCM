ae3fa6b08e7d08180972a903afc57aba6e793c69
yuj168 <71107362+yuj168@users.noreply.github.com>
2021-11-17 00:57:33 +0800

    Update aaip.py

--- parameters/aaip.py
+++ parameters/aaip.py
@@ -6,9 +6,14 @@ class Aaip:
     def __init__(self) :
         self.__lrl=60
         self.__url=120
-        self.__aa=3.5
+        self.__aaReg = 3.5
+        self.__aaUnreg = 3.75
         self.__apw=0.4
+        self.__as = 0.75
         self.__arp=250
+        self.__pvarp = 250
+        self.__hyst = 0
+        self.__rs = 0
 
     def getLRL(self):
         return self.__lrl
@@ -16,14 +21,29 @@ class Aaip:
     def getURL(self):
         return self.__url
     
-    def getAA(self):
-        return self.__aa
+    def getAAReg(self):
+        return self.__aaReg
+
+    def getAAUnreg(self):
+        return self.__aaUnreg
     
     def getAPW(self):
         return self.__apw
+    
+    def getAS(self):
+        return self.__as
 
     def getARP(self):
         return self.__arp
+    
+    def getPVARP(self):
+        return self.__pvarp
+
+    def getHYST(self):
+        return self.__hyst
+    
+    def getRS(self):
+        return self.__rs
         
     def setLRL(self,val):
         if(self.__is_num(val)):
@@ -51,19 +71,33 @@ class Aaip:
                 raise IndexError    
         else:
             raise TypeError
-    def setAA(self,val):
-        if(self.__is_num(val)):
-            num=0.5* round(float(val)/0.5)
-            if(round(float(val),1)<=3.2 and round(float(val),1)>=0.5):
-                self.__aa=round(float(val),1)
-            elif(num<=7.0 and num>=3.5):
-                self.__aa=num
-            elif(round(float(val),1)==0):
-                self.__aa=0
+            
+    def setAAReg(self, val):
+        if (self.__is_num(val)):
+            num = 0.5 * round(float(val) / 0.5)
+            if (round(float(val), 1) <= 3.2 and round(float(val), 1) >= 0.5):
+                self.__aaReg = round(float(val), 1)
+            elif (num <= 7.0 and num >= 3.5):
+                self.__aaReg = num
+            elif (round(float(val), 1) == 0):
+                self.__aaReg = 0
             else:
-                raise IndexError  
+                raise IndexError
         else:
             raise TypeError
+
+    def setAAUnreg(self, val):
+        if (self.__is_num(val)):
+            num = 1.25 * round(float(val) / 1.25)
+            if (round(float(val), 2) <= 5.00 and round(float(val), 2) >= 1.25):
+                self.__aaReg = num
+            elif (round(float(val), 1) == 0):
+                self.__aa = 0
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+            
     def setAPW(self,val):
         if(self.__is_num(val)):
             if(round(float(val),1)<=1.9 and round(float(val),1)>=0.1):
@@ -72,6 +106,7 @@ class Aaip:
                 raise IndexError      
         else:
             raise TypeError
+            
     def setARP(self,val):
         if(self.__is_num(val)):
             if(int(round(float(val),-1))<=500 and int(round(float(val),-1))>=150):
@@ -80,7 +115,42 @@ class Aaip:
                 raise IndexError        
         else:
             raise TypeError
-    
+            
+    def setPVARP(self, val):
+        if (self.__is_num(val)):
+            if (int(round(float(val), -1)) <= 500 and int(round(float(val), -1)) >= 150):
+                self.__pvarp = int(round(float(val), -1))
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def setHYST(self,val):
+        if (self.__is_num(val)):
+            num = 5 * round(float(val) / 5)
+            if (round(float(val)) <= 90 and round(float(val)) >= 50):
+                self.__hyst = round(float(val))
+            elif ((num <= 50 and num >= 30) or (num <= 175 and num >= 90)):
+                self.__hyst = num
+            elif (round(float(val)) == 0):
+                self.__hyst = 0
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+
+    def setRS(self,val):
+        if (self.__is_num(val)):
+            num = 3 * round(float(val) / 3)
+            if (round(float(val)) <= 3 and round(float(val)) >= 21):
+                self.__rs = round(float(val))
+            elif (round(float(val)) == 0):
+                self.__rs = 0
+            else:
+                raise IndexError
+        else:
+            raise TypeError
+            
     def __is_num(self,s):
         try:
             float(s)
