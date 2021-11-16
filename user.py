1bb88e43c66da1f9c65f704d5d7257627a4aef64
pans27 <pans27@mcmaster.ca>
2021-11-16 15:03:18 -0500

    Updated user

--- user.py
+++ user.py
@@ -2,6 +2,12 @@ import parameters.aoop
 import parameters.voop
 import parameters.aaip
 import parameters.vvip
+import parametersA2.aoorp
+import parametersA2.voorp
+import parametersA2.aairp
+import parametersA2.vvirp
+import parametersA2.doop
+import parametersA2.doorp
 #Oct 31, 2021
 #Author: Group 5
 #3k04 DCM assignment 1 user object for storing data
@@ -14,6 +20,12 @@ class User:
         self.aai=parameters.aaip.Aaip()
         self.voo=parameters.voop.Voop()
         self.vvi=parameters.vvip.Vvip()
+        self.aoor=parametersA2.aoorp.Aoorp()
+        self.aair=parametersA2.aairp.Aairp()
+        self.voor=parametersA2.voorp.Voorp()
+        self.vvir=parametersA2.vvirp.Vvirp()
+        self.doo=parametersA2.doop.Doop()
+        self.door=parametersA2.doorp.doorp()
 
     def getUN(self):
         return self.__un
